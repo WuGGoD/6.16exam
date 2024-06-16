5731c707338a8758515b397905aa7047e9c6144b
cavanagh <bartholomewFlash@qq.com>
2024-06-17 00:07:36 +0800

    Updates

--- javascript/detail-BrjUUqen.js
+++ javascript/detail-BrjUUqen.js
@@ -1,4 +1,4 @@
-import{b as p,$ as t}from"./axios-C-GZppl8.js";async function s(){const i=(await p.get(localStorage.getItem("itemOrigin"))).data.items.find(e=>e.item_id.toString()===localStorage.getItem("itemInfo"));t("img").src=i.img,t(".info").innerHTML=`
+import{b as p,$ as t}from"./axios-B1hsza8D.js";async function s(){const i=(await p.get(localStorage.getItem("itemOrigin"))).data.items.find(e=>e.item_id.toString()===localStorage.getItem("itemInfo"));t("img").src=i.img,t(".info").innerHTML=`
             <p class="price">￥${i.price}</p>
             <p><span>价格：<i>￥999</i></span></p>
             <p>${i.title}</p>
@@ -7,4 +7,4 @@ import{b as p,$ as t}from"./axios-C-GZppl8.js";async function s(){const i=(await
                 <p>月销量:${i.sold}件</p>
                 <p>天津</p>
             </div>`}s();
-//# sourceMappingURL=detail-BIym6flJ.js.map
+//# sourceMappingURL=detail-BrjUUqen.js.map
