29b8d16af41751a34bb9cd7529ac9262dc891602
cavanagh <bartholomewFlash@qq.com>
2024-06-16 23:57:53 +0800

    Updates

---
+++ javascript/detail-BIym6flJ.js
@@ -0,0 +1,10 @@
+import{b as p,$ as t}from"./axios-C-GZppl8.js";async function s(){const i=(await p.get(localStorage.getItem("itemOrigin"))).data.items.find(e=>e.item_id.toString()===localStorage.getItem("itemInfo"));t("img").src=i.img,t(".info").innerHTML=`
+            <p class="price">￥${i.price}</p>
+            <p><span>价格：<i>￥999</i></span></p>
+            <p>${i.title}</p>
+            <div class="express">
+                <p>快递：00.00</p>
+                <p>月销量:${i.sold}件</p>
+                <p>天津</p>
+            </div>`}s();
+//# sourceMappingURL=detail-BIym6flJ.js.map
---
+++ javascript/main-DI5Gv-_s.js
@@ -0,0 +1,10 @@
+import{$ as e,a as n,b as l}from"./axios-C-GZppl8.js";import{B as r}from"./core.esm-DWWMfJTk.js";async function t(c){let i=(await l.get(c)).data.items;e(".asscend")&&(i=i.sort((a,o)=>a.price-o.price)),e(".descend")&&(i=i.sort((a,o)=>o.price-a.price)),e(".con").innerHTML=i.map((a,o)=>`
+          <dl data_id=${a.item_id}>
+            <dt><img src="${a.img}" alt=""></dt>
+            <dd>
+              <h2>${a.title}</h2>
+              <h3>月销${a.sold}笔</h3>
+              <p>￥${a.price}</p>
+            </dd>
+          </dl>`).join(""),localStorage.setItem("itemOrigin",c),new r(".wrapper",{scrollY:!0,click:!0})}t("https://zyxcl.xyz/exam_api/zh");document.addEventListener("click",c=>{const s=c.target;if(s.classList.contains("shop")&&(e(".shopActive")&&e(".shopActive").classList.remove("shopActive"),s.classList.add("shopActive")),s.className==="shop compre shopActive"&&t("https://zyxcl.xyz/exam_api/zh"),s.className==="shop sell shopActive"&&t("https://zyxcl.xyz/exam_api/xl"),s.className==="shop new shopActive"&&t("https://zyxcl.xyz/exam_api/sx"),s.className==="arrange"&&(e(".icon-gengduo-2")?(e(".con").className="con grid",e("span",s).className="iconfont icon-fenlei-xuanzhong"):(e(".con").className="con list",e("span",s).className="iconfont icon-gengduo-2")),s.nodeName==="DL"&&(localStorage.setItem("itemInfo",s.getAttribute("data_id")),location.assign("./detail.html")),s.classList.contains("sort")){if(console.log(n("span",s)[0],n("span",s)[1]),e(".active")&&e(".active").classList.remove("active"),e(".asscend")){s.className="sort descend",n("span",s)[1].classList.add("active"),t(localStorage.getItem("itemOrigin"));return}if(e(".descend")){s.className="sort",t(localStorage.getItem("itemOrigin"));return}s.className="sort asscend",n("span",s)[0].classList.add("active"),t(localStorage.getItem("itemOrigin"))}});
+//# sourceMappingURL=main-DI5Gv-_s.js.map
